ed4cf90f3df598f27ebe1c0432200d3d35e51145
ZamNkombisa <149787741+ZamNkombisa@users.noreply.github.com>
2026-02-25 12:34:27 +0200

    added cors for final deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -114,4 +114,8 @@ CORS_ALLOWED_ORIGINS = (
     "http://127.0.0.1:5173",
 )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'your-backend-name.onrender.com',
+    '127.0.0.1', 
+    'localhost'
+]
